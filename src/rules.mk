483b157b50657dac07067aecf31fd0fd88255a22
CyberTwizted <88947585+CyberTwizted@users.noreply.github.com>
2025-05-21 21:58:56 -0500

    Create rules.mk

---
+++ src/rules.mk
@@ -0,0 +1,16 @@
+# Enable features based on keymap.c usage
+MOUSEKEY_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+
+# Split keyboard specific settings
+SPLIT_KEYBOARD = yes
+SPLIT_TRANSPORT = custom
+
+# Highly recommended general features
+BOOTMAGIC_ENABLE = yes
+NKRO_ENABLE = yes
+CONSOLE_ENABLE = yes
+
+# If your Piantor Pro has RGB, you might also see or add:
+# RGBLIGHT_ENABLE = yes
+# OLED_ENABLE = yes # If it has an OLED screen
